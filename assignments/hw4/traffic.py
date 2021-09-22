0c833bf36b276c575e080819823558ac2f5c0094
BurntMallows <Hamiltonqe@g.cofc.edu>
2021-09-22 01:19:07 -0400

    completed hw 4

---
+++ assignments/hw4/traffic.py
@@ -0,0 +1,39 @@
+"""
+Name: Queen Hamilton, II
+<mean>.py
+
+Problem: This program calculates traffic patterns from input
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work
+"""
+
+
+def main():
+    # ask for input: how many roads were surveyed?
+    roads = eval(input("How many roads were surveyed? "))
+    # set total accumulator
+    total = 0
+    # ask for input: how many days was road x surveyed?
+    for _ in range(1, roads + 1):
+        days = eval(input("How many days was road " + str(_) + " surveyed? "))
+        # set daily accumulator
+        acc = 0
+        # ask for input: how many cars traveled on day y? and add
+        for _ in range(1, days + 1):
+            cars = eval(input("     How many cars traveled on day " + str(_) + "? "))
+            acc = acc + cars
+        # calculate total
+        total = total + acc
+        # calculate average per day for each road and print
+        road_average = acc / days
+        print("Road", _, "average vehicles per day:", round(road_average, 2))
+    # calculate the average amount of cars for all the roads
+    total_average = total / roads
+    # print total and total average
+    print("Total number of vehicles traveled on all roads:", total)
+    print("Average number of vehicles per road:", round(total_average, 2))
+
+
+if __name__ == "__main__":
+    main()
